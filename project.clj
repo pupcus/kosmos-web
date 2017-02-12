caede4e10969681465352c999058c74fe839b64c
Michael Pendergrass <mdpendergrass@gmail.com>
2017-02-12 09:20:55 -0500

    update dependencies

--- project.clj
+++ project.clj
@@ -9,13 +9,13 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[kosmos "0.0.3"]
-                 [ring/ring-jetty-adapter "1.4.0"]]
+  :dependencies [[kosmos "0.0.6"]
+                 [ring/ring-jetty-adapter "1.5.1"]]
 
   :profiles {:dev {:resource-paths ["dev-resources"]
                    :dependencies [[org.clojure/clojure "1.8.0"]
-                                  [org.clojure/tools.reader "1.0.0-alpha3"]
-                                  [clj-http "2.1.0"]
+                                  [org.clojure/tools.reader "1.0.0-beta4"]
+                                  [clj-http "2.2.0"]
                                   [org.eclipse.jetty/jetty-servlets "9.2.10.v20150310"]
                                   [org.eclipse.jetty/jetty-xml "9.2.10.v20150310"]]}}
 
