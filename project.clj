d01c06ea5dafbbc05bc0a61a3e1c4924ec94f80b
Michael Pendergrass <mdpendergrass@gmail.com>
2021-01-04 19:52:18 -0500

    update deps

--- project.clj
+++ project.clj
@@ -6,18 +6,20 @@
 
   :scm {:url "git@github.com:pupcus/kosmos-web.git"}
 
+  :signing {:gpg-key "FCA46A30FEEE7E10"}
+
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[kosmos "0.0.11"]
-                 [ring/ring-jetty-adapter "1.8.0" :exclude org.eclipse.jetty/jetty-server]
-                 [org.eclipse.jetty/jetty-server "9.4.24.v20191120"]]
+  :dependencies [[kosmos "0.0.12"]
+                 [ring/ring-jetty-adapter "1.8.2" :exclude org.eclipse.jetty/jetty-server]
+                 [org.eclipse.jetty/jetty-server "9.4.31.v20200723"]]
 
   :profiles {:dev {:resource-paths ["dev-resources"]
                    :dependencies [[org.clojure/clojure "1.10.1"]
-                                  [clj-http "3.10.0"]
-                                  [org.eclipse.jetty/jetty-servlets "9.4.24.v20191120"]
-                                  [org.eclipse.jetty/jetty-xml "9.4.24.v20191120"]]}}
+                                  [clj-http "3.11.0"]
+                                  [org.eclipse.jetty/jetty-servlets "9.4.31.v20200723"]
+                                  [org.eclipse.jetty/jetty-xml "9.4.31.v20200723"]]}}
 
   :test-selectors {:default (complement (some-fn :integration))
                    :integration :integration
